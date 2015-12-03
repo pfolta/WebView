3faea20e724357e3cbcda4aa9159ed513a8e45f4
Peter Folta <mail@peterfolta.net>
2015-12-03 23:02:40 +0000

    Added Platform Class to determine the Current Platform the Application is Running On

---
+++ src/main/java/net/peterfolta/webview/util/Platform.java
@@ -0,0 +1,33 @@
+/*
+ * WebView - A desktop client for web applications
+ * Copyright (C) 2014-2015 Peter Folta. All rights reserved.
+ *
+ * Project:         WebView
+ * Version:         0.0.1
+ * Website:
+ *
+ * File:            Platform.java
+ * Created:         2015-12-03
+ * Author:          Peter Folta <mail@peterfolta.net>
+ */
+
+package net.peterfolta.webview.util;
+
+public final class Platform {
+
+    private Platform() {
+    }
+
+    public static boolean isMac() {
+        return System.getProperty("os.name").toLowerCase().startsWith("mac");
+    }
+
+    public static boolean isLinux() {
+        return System.getProperty("os.name").toLowerCase().startsWith("linux");
+    }
+
+    public static boolean isWindows() {
+        return System.getProperty("os.name").toLowerCase().startsWith("windows");
+    }
+
+}
