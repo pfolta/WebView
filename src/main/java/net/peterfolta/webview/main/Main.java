4e87d88bd2084bb674d4cbf7102548d8fae778dd
Peter Folta <mail@peterfolta.net>
2015-12-12 21:22:44 +0000

    Added First Version of Exit Methods

--- src/main/java/net/peterfolta/webview/main/Main.java
+++ src/main/java/net/peterfolta/webview/main/Main.java
@@ -13,12 +13,35 @@
 
 package net.peterfolta.webview.main;
 
+import net.peterfolta.webview.view.common.MessageBox;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Shell;
+
 public class Main {
 
     public static void main(String[] args) {
         if (args.length > 0) {
             new WebViewDriver(args[0]);
         }
+    }
+
+    public static void exitWithException(String errorMessage, Exception exception) {
+        // Format Error Message to include Details
+        errorMessage += System.lineSeparator();
+        errorMessage += System.lineSeparator();
+        errorMessage += exception.getClass().getName() + ": " + exception.getMessage();
+
+        // Print Stack Trace
+        exception.printStackTrace();
+
+        // Display GUI Error Message
+        new MessageBox(new Shell(), errorMessage, "WebView", SWT.ICON_ERROR, SWT.OK);
+
+        Main.exit(-1);
+    }
+
+    public static void exit(int status) {
+        System.exit(status);
     }
 
 }
