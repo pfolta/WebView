44710b1522886489f598f927b0b855bf1f1a4a10
Peter Folta <mail@peterfolta.net>
2015-12-12 21:19:19 +0000

    Added application.properties and Corresponding Application Class

---
+++ src/main/java/net/peterfolta/webview/main/Application.java
@@ -0,0 +1,67 @@
+/*
+ * WebView - A desktop client for web applications
+ * Copyright (C) 2014-2015 Peter Folta. All rights reserved.
+ *
+ * Project:         WebView
+ * Version:         0.0.1
+ * Website:
+ *
+ * File:            Application.java
+ * Created:         2015-12-12
+ * Author:          Peter Folta <mail@peterfolta.net>
+ */
+
+package net.peterfolta.webview.main;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public final class Application {
+
+    private static Application instance;
+
+    private Properties applicationProperties;
+
+    private Application() {
+        try {
+            InputStream inputStream = this.getClass().getResourceAsStream("/application.properties");
+
+            if (inputStream == null) {
+                throw new IOException("File does not exist.");
+            }
+
+            applicationProperties = new Properties();
+            applicationProperties.load(inputStream);
+
+            inputStream.close();
+        } catch (IOException exception) {
+            Main.exitWithException("An error occurred while trying to load application.properties.", exception);
+        }
+    }
+
+    public static Application getInstance() {
+        if (instance == null) {
+            instance = new Application();
+        }
+
+        return instance;
+    }
+
+    public String getGroupId() {
+        return applicationProperties.getProperty("application.groupId");
+    }
+
+    public String getArtifactId() {
+        return applicationProperties.getProperty("application.artifactId");
+    }
+
+    public String getName() {
+        return applicationProperties.getProperty("application.name");
+    }
+
+    public String getVersion() {
+        return applicationProperties.getProperty("application.version");
+    }
+
+}
